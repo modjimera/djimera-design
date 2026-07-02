b523a46c4bb5e5547cc51b546fb885a313e17f8d
methdjim <mmdjimera@gmail.com>
2026-07-02 21:30:09 +0000

    Fix production database seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -17,9 +18,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::query()->updateOrCreate([
+            'email' => 'admin@djimera-design.com',
+        ], [
+            'name' => 'Administrateur Djimera',
+            'role' => 'Administrateur',
+            'telephone' => '+221 77 000 00 00',
+            'statut' => 'Actif',
+            'password' => Hash::make('password'),
         ]);
     }
 }
